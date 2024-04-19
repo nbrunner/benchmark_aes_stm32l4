86f7404a63bec14446b4be6d3735d75716ea1a17
Nicolas Brunner <nicolas.brunner@heig-vd.ch>
2024-04-19 09:50:28 +0200

    Use snake_case

--- Core/Inc/aes_hw.h
+++ Core/Inc/aes_hw.h
@@ -27,13 +27,13 @@ void aes_hw_init(void);
 /**
  * Encrypt using AES in CTR Mode
  * @param key the 128 bits key used for AES algorithm.
- * @param initVector Initialization Vector used for AES algorithm.
- * @param plainData pointer to the data to encrypt
+ * @param init_vector Initialization Vector used for AES algorithm.
+ * @param plain_data pointer to the data to encrypt
  * @param length the length of the data to encrypt in byte
- * @param cipherData: pointer to the encrypted data
+ * @param cipher_data: pointer to the encrypted data
  * @return true if operation success
  */
-bool aes_hw_ctr_encrypt(uint8_t* key, uint8_t* initVector, const uint8_t* plainData, uint32_t length, uint8_t* cipherData);
+bool aes_hw_ctr_encrypt(uint8_t* key, uint8_t* init_vector, const uint8_t* plain_data, uint32_t length, uint8_t* cipher_data);
 
 // AES CTR decryption is the same than encryption
 #define aes_hw_ctr_decrypt aes_ctr_encrypt
